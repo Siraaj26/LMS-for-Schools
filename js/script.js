49e232a12aa2958e23f4add61920adbf5e8e9529
pakerencpt024 <pakerencpt024@student.wethinkcode.co.za>
2025-09-20 19:00:22 +0200

    Inserted footer.html into #footer and made changes to styling

--- js/script.js
+++ js/script.js
@@ -9,5 +9,16 @@ function loadNavbar() {
     .catch(error => console.error('Error loading the navbar:', error));
 }
 
+// Function to load the footer component
+function loadFooter(){
+    fetch('../components/footer.html')
+    .then(response => response.text())
+    .then(data => {
+    document.getElementById('footer').innerHTML = data;
+    })
+    .catch(error => console.error('Error loading the footer:', error));
+}
+
 // Call the function to load the navbar when the script runs
 loadNavbar();
+loadFooter();
