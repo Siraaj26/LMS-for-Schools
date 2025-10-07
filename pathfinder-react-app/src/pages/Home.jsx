1d3926dd427f7852631d16300df1dbca8fae8bc7
Siraaj Jeftha <sirjeftha@gmail.com>
2025-10-07 13:38:48 +0200

    removing unneccesary css files

--- pathfinder-react-app/src/pages/Home.jsx
+++ pathfinder-react-app/src/pages/Home.jsx
@@ -222,34 +222,8 @@ function Home() {
                 </div>
             </section>
 
-            <footer className="footer">
-                <div className="container">
-                    <div className="footer-content">
-                        <h3>Pathfinder Vanguard</h3>
-                        
-                        <p>Follow us on social media</p>
-                        <div className="footer-social">
-                            <a href="https://www.instagram.com/empirasglobalacademy/?hl=en" target="_blank" rel="noopener noreferrer" className="social-icon">
-                                <i className="fab fa-instagram"></i>
-                            </a>
-                            <a href="https://www.facebook.com/empirasglobalacademy" target="_blank" rel="noopener noreferrer" className="social-icon">
-                                <i className="fab fa-facebook"></i>
-                            </a>
-                            <a href="https://x.com/empirasglobal/status/1962431564660531362" target="_blank" rel="noopener noreferrer" className="social-icon">
-                                <i className="fab fa-twitter"></i>
-                            </a>
-                            <a href="https://www.linkedin.com/company/empiras/posts/?feedView=all" target="_blank" rel="noopener noreferrer" className="social-icon">
-                                <i className="fab fa-linkedin"></i>
-                            </a>
-                        </div>
-                        
-                        <div className="footer-bottom">
-                            <p>&copy; 2025 Pathfinder Global Academy. All rights reserved.</p>
-                        </div>
-                    </div>
-                </div>
-            </footer>
             </main>
+            <Footer />
         </>
     );
 }
